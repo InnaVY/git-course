981f16333db2659c0f36be4f5e50e6cd1525dbbf
inna.yatsyk <inna.yatsyk@globallogic.com>
2021-12-10 19:15:10 +0200

    FOR PUSH

--- src/main/java/HelloGit1.java
+++ src/main/java/HelloGit1.java
@@ -1,5 +1,16 @@
 public class HelloGit1 {
-    public static void main(String [] main){
-    System.out.println();
+    public static void main(String[] args) {
+        int[] a= new int[10];
+        for (int i : a) {
+            System.out.println("i");
+
+
+            System.out.println("fOR COMMIT");
+
+
+            System.out.println("QWERTT");
+
+        }
     }
+
 }
